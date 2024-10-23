95530a4002e149246a7dda5683dd35ceefed95d7
kairong0413 <liankairong888@gmail.com>
2024-10-23 21:21:20 +0800

    BMI

---
+++ StudentsFiles/LIANKAIRONG/lab1BMI.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    double height, weight, BMI;
+    cout <<"Enter your weight(in kg) : ";
+    cin >> weight;
+    cout <<"Enter your height(in meters) : ";
+    cin >> height;
+    BMI = weight/(height*height);
+    cout <<"Your BMI is: "<<BMI;
+
+    return 0;
+}
