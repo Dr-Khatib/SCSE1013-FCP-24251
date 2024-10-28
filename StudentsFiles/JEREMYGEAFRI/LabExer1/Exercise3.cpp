2ad82c458659ca7ad418103fae8a2fba21c13130
Jeremy Geafri <jeremy.geafri05@gmail.com>
2024-10-28 21:24:20 +0800

    Update Exercise3.cpp

--- StudentsFiles/JEREMYGEAFRI/LabExer1/Exercise3.cpp
+++ StudentsFiles/JEREMYGEAFRI/LabExer1/Exercise3.cpp
@@ -3,13 +3,13 @@ using namespace std;
 int main()
 {
     cout << "Enter an integer: " << endl;
-    int num=0 ;
+    int num ;
     cin >> num ;
     if (num > 0)
     cout << num << " is a positive integer" << endl ;
     else if(num<0)
     cout << num << " is a negative integer" << endl ;
-    else (num=0);
+    else ;
     cout << num << " is zero" << endl ;
     return 0 ;
 }
