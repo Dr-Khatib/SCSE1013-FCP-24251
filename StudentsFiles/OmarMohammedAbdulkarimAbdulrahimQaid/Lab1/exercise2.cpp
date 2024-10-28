d48648b56f4d1f26e4c009e30655f431feaecc71
Omar <74186207+Oshrjabi@users.noreply.github.com>
2024-10-28 23:06:29 +0800

    Update exercise2.cpp

--- StudentsFiles/OmarMohammedAbdulkarimAbdulrahimQaid/Lab1/exercise2.cpp
+++ StudentsFiles/OmarMohammedAbdulkarimAbdulrahimQaid/Lab1/exercise2.cpp
@@ -6,26 +6,20 @@ using namespace std;
 int main() {
     double num1, num2;
 
-    // Ask the user for two numbers
     cout << "Enter two numbers: ";
     cin >> num1 >> num2;
 
-    // Set the decimal precision to 2
     cout << fixed << setprecision(2);
 
-    // Calculate and display the sum
     double sum = num1 + num2;
     cout << "Sum: " << sum << endl;
 
-    // Calculate and display the difference
     double difference = num1 - num2;
     cout << "Difference: " << difference << endl;
 
-    // Calculate and display the product
     double product = num1 * num2;
     cout << "Product: " << product << endl;
 
-   // Calculate and display the quotient
     if (num2 != 0) {
     double quotient = num1 / num2; 
     cout << "Quotient: " << quotient << endl;
