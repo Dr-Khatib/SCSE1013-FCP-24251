e373664ae33f53f236e7eae565699f3dba73f292
Omar <74186207+Oshrjabi@users.noreply.github.com>
2024-10-28 23:07:03 +0800

    exercise1.cpp

--- StudentsFiles/OmarMohammedAbdulkarimAbdulrahimQaid/Lab1/exercise1.cpp
+++ StudentsFiles/OmarMohammedAbdulkarimAbdulrahimQaid/Lab1/exercise1.cpp
@@ -6,18 +6,14 @@ int main() {
     int BirthYear;
     const int CurrentYear = 2024;
 
-    //The program asks the user for their full name
     cout << "Enter your full name: ";
     getline(cin,FullName);
 
-    //The program asks the user for their birthyear
     cout << "Enter your birthyear: ";
     cin >> BirthYear;
 
-    //The program calculates the age of the user
     int age = CurrentYear - BirthYear;
 
-    //The program displays the age of the user
     cout << ("Hello, ") << FullName << ("! You are ") << age << (" years old.");
 
 
