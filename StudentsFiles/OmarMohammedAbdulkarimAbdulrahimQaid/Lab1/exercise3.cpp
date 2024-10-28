10e218ddbf64a49d149ab5dc5853208a42a92dbf
Omar <74186207+Oshrjabi@users.noreply.github.com>
2024-10-28 23:08:31 +0800

    exercise3.cpp

--- StudentsFiles/OmarMohammedAbdulkarimAbdulrahimQaid/Lab1/exercise3.cpp
+++ StudentsFiles/OmarMohammedAbdulkarimAbdulrahimQaid/Lab1/exercise3.cpp
@@ -4,11 +4,9 @@ using namespace std;
 int main() {
     int n;
 
-    // Ask the user to input an integer
     cout << "Enter an integer: ";
     cin >> n;
 
-    // Check if the number is positive, negative, or zero
     if (n > 0) {
         cout << n << " is positive." << endl;
     } else if (n < 0) {
