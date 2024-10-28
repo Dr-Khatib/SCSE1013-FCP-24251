e02696bda47109009815e1007715dce7f1f89be7
njwongzhe <ngjaawei@gmail.com>
2024-10-28 15:20:32 +0800

    Update LAB EXERCISE 1 - 1. Enhanced Input and Output.cpp
    Correction

--- StudentsFiles/NGJAAWEI/LAB EXERCISES 1/LAB EXERCISE 1 - 1. Enhanced Input and Output.cpp
+++ StudentsFiles/NGJAAWEI/LAB EXERCISES 1/LAB EXERCISE 1 - 1. Enhanced Input and Output.cpp
@@ -5,11 +5,14 @@ using namespace std;
 int main ()
 {
     char name[100];
-    int birth_year;
+    int birth_year, age;
 
     cout << "Enter your full name: " ; cin.getline(name, 100);
     cout << "Enter your birth year: "; cin >> birth_year;
-    cout << "Hello, " << name << "! You are " << birth_year << " years old.";
+    
+    age = 2024 - birth_year;
+    
+    cout << "Hello, " << name << "! You are " << age << " years old.";
 
     return 0;
 }
