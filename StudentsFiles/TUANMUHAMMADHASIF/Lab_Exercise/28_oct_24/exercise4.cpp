4d5c229e3d3a71597f3a4e7eb9078b521dd0c573
TUAN MUHAMMAD HASIF BIN TUAN ZAKI <t.hhasiff@gmail.com>
2024-10-28 15:28:37 +0800

    Update exercise4.cpp

--- StudentsFiles/TUANMUHAMMADHASIF/Lab_Exercise/28_oct_24/exercise4.cpp
+++ StudentsFiles/TUANMUHAMMADHASIF/Lab_Exercise/28_oct_24/exercise4.cpp
@@ -8,14 +8,24 @@ int main()
 	cout << "Enter three numbers:";
 	cin >> input1 >> input2 >> input3;
 	
-	if (input1 >= input2 && input1 >= input3)
-		cout << "" << input1 << " is the largest (or tied for largest)";
+	if (input1 > input2 && input1 > input3)
+		cout << "The first number, " << "" << input1 << " is the largest";
+	
+	else if (input2 > input1 && input2 > input3)
+		cout << "The second number, " << "" << input1 << " is the largest";
 		
-	else if (input2 >= input1 && input2 >= input3)
-		cout << "" << input2 << " is the largest (or tied for largest)";
+	else if (input3 > input2 && input3 > input1)
+		cout << "The first number, " << "" << input1 << " is the largest";
+	
+	else if (input1 == input2 && input1 > input3)
+		cout << "The first and second numbers are tied for the largest which is " << input1;
 		
-	else if (input3 >= input2 && input3 >= input2)
-		cout << "" << input3 << " is the largest (or tied for largest)";
+	else if (input1 == input3 && input1 > input2)
+		cout << "The first and third numbers are tied for the largest which is " << input1;		
+		
+	else if (input2 == input3 && input2 > input1)
+		cout << "The second and third numbers are tied for the largest which is " << input2;
+		
 	
 	return 0;
 }
