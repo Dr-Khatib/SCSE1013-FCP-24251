d40d33e30d351301e2b4728ccb78ffa3a550fb09
CHING LEONG CHEN <leonchingleongchen@gmail.com>
2024-11-04 09:54:15 +0800

    labexercise

---
+++ StudentsFiles/CHINGLEONGCHEN/LABEXERCISE/exer4.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std ;
+
+
+
+int main() {
+
+ int num1, num2, num3;
+
+    
+    cout << "Enter three numbers: ";
+    cin >> num1 >> num2 >> num3;
+
+   
+    int highest = num1;
+
+    
+    if (num2 > highest) {
+        highest = num2;
+    }
+
+    if (num3 > highest) {
+        highest = num3;
+    }
+
+    cout << "The highest number is: " << highest << endl;
+
+    system("pause");
+
+
+    return 0;
+}
+
