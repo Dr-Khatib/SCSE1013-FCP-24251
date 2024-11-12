34eb165747922d1651c67be581bf3bc680d13a7c
MD EMON <mdemon22830@gmail.com>
2024-11-12 17:22:28 +0800

    Add files via upload

---
+++ StudentsFiles/MD_EMON/LabExer2/Exercise3.cpp
@@ -0,0 +1,21 @@
+/*Exercise 3: Basic Function Definition
+This exercise introduces how to declare and define a basic function:
+Define a function named greet that takes no parameters and outputs "Hello, World!". Then, call this function from main().
+*/
+
+
+#include <iostream>
+
+using namespace std;
+
+void greet(); // Function declaration
+
+int main() {
+    greet(); // Call the greet function
+    return 0;
+}
+
+// Function definition
+void greet() {
+    cout << "Hello, World!" << endl;
+}
