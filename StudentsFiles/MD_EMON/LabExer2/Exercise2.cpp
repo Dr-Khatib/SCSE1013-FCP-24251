79b4aa5bb4de5a400e1371ba02d98664588da9cf
MD EMON <mdemon22830@gmail.com>
2024-11-12 17:22:02 +0800

    Add files via upload

---
+++ StudentsFiles/MD_EMON/LabExer2/Exercise2.cpp
@@ -0,0 +1,21 @@
+/*Exercise 2: Library Function Usage
+Complete the program by entering a number and observing the use of the library function:
+*/
+
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    double num, powered;
+    cout << "Enter a number to powered: ";
+    cin >> num;
+
+    powered = pow(num,num);
+    cout << "The power of " <<powered<<endl;
+
+    return 0;
+}
+
