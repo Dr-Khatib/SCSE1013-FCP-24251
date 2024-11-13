14a0035c46dd7c6ed93e4a6e59ed0c0f256e347a
adzarf <aqildzarfan@protonmail.com>
2024-11-13 13:37:22 +0800

    commit Exercise

--- StudentsFiles/Aqil_Dzarfan/LabExer2/Exercise 1.cpp
+++ StudentsFiles/Aqil_Dzarfan/LabExer2/Exercise 1.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
-#include <cmath>//library functions
+#include <cmath> //library functions
 
 using namespace std;
 
-void greet(); //function prototype
+void greet(); // function prototype
 
-int main() {
-    double result = sqrt(16.0); 
+int main()
+{
+    double result = sqrt(16.0);
     cout << "Square root of 16 is: " << result << endl;
 
     greet(); // Calling the user-defined function
     return 0;
 }
 
-void greet() //user defined function 
+void greet() // user defined function
 {
     cout << "Hello from greet function!" << endl;
 }
---
+++ StudentsFiles/Aqil_Dzarfan/LabExer2/Exercise 3.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+// Function declaration
+void greet()
+{
+    cout << "Hello World!";
+}
+
+int main()
+{
+    greet(); // Call the greet function
+    return 0;
+}
